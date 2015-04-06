59bdaee0e842d15f803d874cc856009bf617bcc8
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-04-06 13:25:39 +0200

    Extracting auxiliary functions.

--- move.ml
+++ move.ml
@@ -11,65 +11,62 @@ let range ~piece =
     | Board.PieceTwo -> 2
     | Board.PieceThree -> 3
 
-let stops ~board ~from ~distance =
-  let find_candidate_cells step =
-    let is_previous cell =
-      match step.history with
-        | [] -> false
-        | last_step::_ -> Board.compare cell last_step.current
-    in
-    let is_loop cell =
-      let prev = step.current
-      and remaining = step.remaining - 1 in
-      let is_loop_step past_step =
-        match past_step.history with
-          | [] -> false
-          | prev_past_step::_ ->
-              let same_cell = Board.compare cell past_step.current
-              and same_origin = Board.compare prev prev_past_step.current
-              and same_remaining = remaining = past_step.remaining in
-              same_cell && same_origin && same_remaining
-      in
-      List.exists is_loop_step step.history
-    in
-    let candidate_selector cell =
-      if is_previous cell then false
-      else if is_loop cell then false
-      else
-        match cell.position, step.remaining with
-          | Top, 1 | Bot, 1 -> true
-          | Top, _ | Bot, _ -> false
-          | _, 1 -> true
-          | _, _ -> Board.is_empty ~board ~position:cell.position
-    in
-    List.filter candidate_selector step.current.edges
+let find_next_cells ~board ~step =
+  let is_previous cell =
+    match step.history with
+      | [] -> false
+      | last_step::_ -> Board.compare cell last_step.current
   in
+  let is_loop cell =
+    let prev = step.current
+    and remaining = step.remaining - 1 in
+    let is_loop_step past_step =
+      match past_step.history with
+        | [] -> false
+        | prev_past_step::_ ->
+            let same_cell = Board.compare cell past_step.current
+            and same_origin = Board.compare prev prev_past_step.current
+            and same_remaining = remaining = past_step.remaining in
+            same_cell && same_origin && same_remaining
+    in
+    List.exists is_loop_step step.history
+  in
+  let candidate_selector cell =
+    if is_previous cell then false
+    else if is_loop cell then false
+    else
+      match cell.position, step.remaining with
+        | Top, 1 | Bot, 1 -> true
+        | Top, _ | Bot, _ -> false
+        | _, 1 -> true
+        | _, _ -> Board.is_empty ~board ~position:cell.position
+  in
+  List.filter candidate_selector step.current.edges
+
+let stops ~board ~from ~distance =
   let rec walk stops explorable =
     match explorable with
       | [] -> stops
-      | step::rest ->
+      | step::rest when step.remaining = 0 ->
           begin
-            match step.remaining with
-              | 0 ->
-                  begin
-                    match Board.piece_at ~board ~position:step.current.position with
-                      | None -> walk (step.current::stops) rest
-                      | Some piece ->
-                          let jump = { step with remaining = (range ~piece) } in
-                          walk stops (jump::rest)
-                  end
-              | n ->
-                  let build_step cell =
-                    {
-                      current = cell ;
-                      history = step::step.history ;
-                      remaining = n - 1 ;
-                    }
-                  in
-                  let candidate_cells = find_candidate_cells step in
-                  let neighbors = List.map build_step candidate_cells in
-                  walk stops (neighbors @ rest)
+            match Board.piece_at ~board ~position:step.current.position with
+              | None ->
+                  walk (step.current::stops) rest
+              | Some piece ->
+                  let jump = { step with remaining = (range ~piece) } in
+                  walk stops (jump::rest)
           end
+      | step::rest ->
+          let build_step cell =
+            {
+              current = cell ;
+              history = step::step.history ;
+              remaining = step.remaining - 1 ;
+            }
+          in
+          let candidate_cells = find_next_cells ~board ~step in
+          let neighbors = List.map build_step candidate_cells in
+          walk stops (neighbors @ rest)
   in
   let initial_step =
     {
